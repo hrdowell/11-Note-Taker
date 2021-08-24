41fdfc8c65b2e4c6208ab8f88997066056f5de17
hrdowell <hannahrdowell@gmail.com>
2021-08-23 23:44:45 -0500

    server.js heroku added

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 // Dependencies
 // =============================================================
+const cool = require("cool-ascii-faces");
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
+//Sets up Heroku App
+//==============================================================
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // Sets up the Express App
 // =============================================================
